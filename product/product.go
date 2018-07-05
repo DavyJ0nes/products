69ff08a5d883e0f3935eba77bf39a7fcbd824ebd
DavyJ0nes <davy.jones@me.com>
2018-07-05 07:53:41 +0100

    Add initial data structures

--- product/product.go
+++ product/product.go
@@ -1,5 +1,32 @@
 package product
 
+type Product struct {
+	ID          string
+	Name        string
+	Description string
+	Price       float64
+	Regions     []Region
+}
+
+type Region struct {
+	Name     string
+	Currency string
+	Taxes    []Tax
+}
+
+type Tax struct {
+	Name   string
+	Amount float64
+}
+
+type Currency struct {
+	Name        string
+	CountryName string
+	Symbol      rune
+}
+
+type Currencies []Currency
+
 func exampleFunction(s string) int {
 	return len(s)
 }
--- transaction/transaction.go
+++ transaction/transaction.go
@@ -1,5 +1,20 @@
 package transaction
 
+import (
+	"github.com/davyj0nes/products-api/product"
+	"time"
+)
+
+type Transaction struct {
+	ID         string
+	Datetime   time.Time
+	RegionName string
+	Products   []product.Product
+	Subtotal   float64
+	TaxTotal   float64
+	Total      float64
+}
+
 func exampleFunction(s string) int {
 	return len(s)
 }
