66cfe6e4d1481ef4d96a111839f5a7d634d9b8af
DavyJ0nes <davy.jones@me.com>
2018-07-06 17:57:52 +0100

    Add Prometheus Monitoring to All Requests

--- api/handlers/handlers.go
+++ api/handlers/handlers.go
@@ -3,6 +3,11 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+
+	"github.com/felixge/httpsnoop"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	"github.com/davyj0nes/products/api/models"
 	"github.com/gorilla/mux"
@@ -13,8 +18,24 @@ type Error struct {
 	Message string `json:"message,omitempty"`
 }
 
+var (
+	httpRequestsResponseTime prometheus.Summary
+	requestDuration          *prometheus.HistogramVec
+)
+
+func init() {
+	// initialise request duration metric
+	requestDuration = prometheus.NewHistogramVec(prometheus.HistogramOpts{
+		Name:    "request_duration_seconds",
+		Help:    "Time (in secods) spent serving HTTP requests",
+		Buckets: prometheus.DefBuckets,
+	}, []string{"method", "route", "status_code"})
+
+	prometheus.MustRegister(requestDuration)
+}
+
 // Router is the mux Router for the Service
-func Router(buildTime, commit, release string) *mux.Router {
+func Router(buildTime, commit, release string) http.Handler {
 	// Setting up in memeory data
 	models.Seed()
 
@@ -34,7 +55,11 @@ func Router(buildTime, commit, release string) *mux.Router {
 	r.HandleFunc("/api/v1/transaction/all", allTransactions).Methods("GET")
 	r.HandleFunc("/api/v1/transaction/{id}", getTransaction).Methods("GET")
 
-	return r
+	// prometheus metrics endpoint
+	r.Handle("/metrics", promhttp.Handler())
+
+	withMetrics := middleware(r)
+	return withMetrics
 }
 
 // generateJSONResponse is a helper to allow for easier output of JSON data
@@ -43,4 +68,14 @@ func generateJSONResponse(w http.ResponseWriter, status int, data interface{}) {
 	w.WriteHeader(status)
 	encoder := json.NewEncoder(w)
 	encoder.Encode(data)
+}
+
+// middleware runs a set of functions on every request.
+func middleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			m := httpsnoop.CaptureMetrics(next, w, r)
+
+			requestDuration.WithLabelValues(r.Method, r.URL.Path, strconv.Itoa(m.Code)).Observe(m.Duration.Seconds())
+		})
 }
