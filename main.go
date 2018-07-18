46450b25df7834785bc0ca3b3b5071b88ffacf36
DavyJ0nes <davy.jones@me.com>
2018-07-18 10:41:14 +0100

    Set graceful shutdown and Read/Write timeouts

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/davyj0nes/products/api/handlers"
 	"github.com/davyj0nes/products/api/version"
@@ -22,13 +26,41 @@ func main() {
 	port := flag.String("port", "8080", "port number to use with Web Service")
 	flag.Parse()
 
+	// Create router with attached handlers
 	log.Printf("Starting Service... | {commit: %s, build_time: %s, release: %s}", version.Commit, version.BuildTime, version.Release)
 	router := handlers.Router(version.BuildTime, version.Commit, version.Release)
 
+	// srv object sets up prod grade http sever
+	// for more info see: https://blog.cloudflare.com/the-complete-guide-to-golang-net-http-timeouts
+	//   && https://blog.gopheracademy.com/advent-2016/exposing-go-on-the-internet/
 	srv := &http.Server{
-		Addr:    ":" + *port,
-		Handler: router,
+		Addr:         ":" + *port,
+		Handler:      router,
+		ReadTimeout:  3 * time.Second,
+		WriteTimeout: 5 * time.Second,
 	}
 
-	log.Fatal(srv.ListenAndServe())
+	// create channel to handle sigterm signal for graceful shutdown
+	interrupt := make(chan os.Signal, 1)
+	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		log.Fatal(srv.ListenAndServe())
+	}()
+
+	log.Info("Service has started...")
+
+	killSig := <-interrupt
+	switch killSig {
+	case os.Interrupt:
+		log.Info("Received CTRL+C Interrupt Signal")
+
+	case syscall.SIGTERM:
+		log.Info("Received SIGTERM, Terminating...")
+	}
+
+	log.Info("Service shutting down ...")
+	srv.Shutdown(context.Background())
+	log.Info("Service has shut down")
+
 }
